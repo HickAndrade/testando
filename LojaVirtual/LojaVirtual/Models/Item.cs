87798fea55a5cf57bc3f40925f40408ba314cee4
HickAndrade <henrique_andrade2013@hotmail.com>
2020-03-18 12:58:44 -0300

    teste

---
+++ LojaVirtual/LojaVirtual/Models/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Models
+{
+    public class Item
+    {
+        [Display(Name = "Codigo do Produto")]
+        public int ItemId { get; set; }
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+        [Display(Name ="Descrição")]
+        public string Descricao { get; set; }
+
+        public Item()
+        {
+        }
+
+        public Item(int itemId, string nome, double valor, string descricao)
+        {
+            ItemId = itemId;
+            Nome = nome;
+            Valor = valor;
+            Descricao = descricao;
+        }
+    }
+}
---
+++ LojaVirtual/LojaVirtual/Persistences/LojaContext.cs
@@ -0,0 +1,21 @@
+﻿using LojaVirtual.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Persistences
+{
+    public class LojaContext : DbContext
+    {
+        public DbSet<Item> Itens { get; set; }
+
+
+        public LojaContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+    }
+}
